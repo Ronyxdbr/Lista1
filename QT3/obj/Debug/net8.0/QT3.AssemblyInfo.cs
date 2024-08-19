fba39a34c74ccac023360fb7cc6e22b44a16d713
Roney <miltonroneynobre@gmail.com>
2024-08-19 17:49:02 -0300

    Resolução de questão

--- QT3/obj/Debug/net8.0/QT3.AssemblyInfo.cs
+++ QT3/obj/Debug/net8.0/QT3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QT3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a1fda4e6be9b710f4096a213b2ae8d13cb3c455")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb3ffb101dab82de6e5285c24428b00cddfa80df")]
 [assembly: System.Reflection.AssemblyProductAttribute("QT3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QT3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
